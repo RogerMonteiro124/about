a3424ad69acbc4fff34c86c4b61a6e9495c21ca0
Roger Monteiro <roger-15monteiro@hotmail.com>
2021-09-12 17:57:47 -0400

    'addlinkedin'

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/rogermonteiro124"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
         </a>
       </header>
     </div>
